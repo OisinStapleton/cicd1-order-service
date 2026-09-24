455022614cc859741aed336b08d8d0ceefc7a0bc
OisinStapleton <G00365445@atu.ie>
2026-09-24 08:47:59 +0100

    Updated Application properties & converted purchaseorder into an entity

--- src/main/java/ie/atu/cicd1orderservice/model/PurchaseOrder.java
+++ src/main/java/ie/atu/cicd1orderservice/model/PurchaseOrder.java
@@ -3,12 +3,18 @@ package ie.atu.cicd1orderservice.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "purchase_orders")
 public class PurchaseOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long productId;
     private int quantity;
 }
+
